568eae7a7d71a94c229c2c3bfe010bb881d75f5b
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-03 08:11:50 +0530

    SpringBean 1,2 is Created

---
+++ HelloSpring/src/main/java/lk/ijse/gdse66/spring/SpringBean2.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse66.spring;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Spring
+ * @since : 8:02 AM - 2/3/2024
+ **/
+public class SpringBean2 {
+    public SpringBean2() {
+        System.out.println("SpringBean2 instance is created");
+    }
+}
