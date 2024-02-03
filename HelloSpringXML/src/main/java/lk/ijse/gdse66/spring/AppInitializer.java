8095e029d1af73ab84a33b21cb7061311b5ed5a6
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-03 10:38:57 +0530

    XML Configuration Modified

--- HelloSpringXML/src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
+++ HelloSpringXML/src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
@@ -9,6 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  **/
 public class AppInitializer {
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext("context.xml");
+        ClassPathXmlApplicationContext xmlac = new ClassPathXmlApplicationContext("context.xml");
+
+        SpringBean springBean = xmlac.getBean(SpringBean.class);
+        System.out.println(springBean);
     }
 }
