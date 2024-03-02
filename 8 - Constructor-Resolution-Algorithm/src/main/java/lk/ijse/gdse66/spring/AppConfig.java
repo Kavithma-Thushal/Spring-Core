b91e4f8423cb3a1cd02b1b0966a0c042e2c292f9
Thushal2001 <kavithmathushal9007@gmail.com>
2024-03-02 09:56:41 +0530

    README.md Updated 2

--- 8 - Constructor-Resolution-Algorithm/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
+++ 8 - Constructor-Resolution-Algorithm/src/main/java/lk/ijse/gdse66/spring/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class AppConfig {
+
     @Bean
     public String myString() {
         return "Amal";
